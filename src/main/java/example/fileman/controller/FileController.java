68831b2af0832a86363c99376bf826a5a514f370
Brian <cricketfeir@gmail.com>
2017-04-29 15:12:39 -0400

    Added code for fetching metadata

--- src/main/java/example/fileman/controller/FileController.java
+++ src/main/java/example/fileman/controller/FileController.java
@@ -1,6 +1,7 @@
 package example.fileman.controller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,17 +27,31 @@ public class FileController {
 	
 	@CrossOrigin
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
-	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){		
-		
-		
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("owner") String owner){			
 		try {
-			fileService.saveFile(file, "test owner");
+			fileService.saveFile(file, owner);
 			return new ResponseEntity<String>("successful file upload",HttpStatus.OK);
 		} catch (IllegalStateException | IOException e) {
 			e.printStackTrace();
 			return new ResponseEntity<String>("failure in saving file",HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
+		}		
 	}
-		
+	
+	@CrossOrigin
+	@RequestMapping(value="/metaData", method=RequestMethod.GET)
+	public ResponseEntity<String> uploadFile(@RequestParam("file") String file, @RequestParam("name") String name) throws FileNotFoundException{			
+		try{
+			String retval = fileService.getMetaData(file, name);	
+			if(retval == null){
+				return new ResponseEntity<String>(retval,HttpStatus.NOT_FOUND);
+			}else{
+				return new ResponseEntity<String>(retval,HttpStatus.OK);
+			}
+		}catch(FileNotFoundException e){
+			return new ResponseEntity<String>("File not found on server",HttpStatus.NOT_FOUND);
+		}catch(Exception e){
+			e.printStackTrace();
+			return new ResponseEntity<String>("",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
--- src/main/java/example/fileman/service/FileService.java
+++ src/main/java/example/fileman/service/FileService.java
@@ -1,7 +1,11 @@
 package example.fileman.service;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
@@ -15,15 +19,24 @@ import org.springframework.web.multipart.MultipartFile;
 @EnableScheduling
 public class FileService {
 	
+	private final static String DELIMITER = ":=";
+	
 	public void saveFile(MultipartFile file, String owner) throws IllegalStateException, IOException{
 		
 		//basic measure to keep people from messing with the file structure
 		String modifiedFilename = file.getOriginalFilename().replaceAll("'\'", "");
-		String modifiedPath = System.getenv("FILEPATH").replaceAll("'\'", "\\");
+		
+		//if the filename will cause a collision with the metadata, stop now
+		if(modifiedFilename.equals("metadata.txt")){
+			throw new IOException();
+		}
+		
+		String modifiedPath = getModifiedPath();
+		int periodIndex = modifiedFilename.indexOf('.')==-1?modifiedFilename.length():modifiedFilename.indexOf('.');
 		
 		System.out.println(modifiedFilename);
-		File folder = new File(modifiedPath+modifiedFilename.substring(0, modifiedFilename.indexOf('.')));
-		File toAdd = new File(modifiedPath+modifiedFilename.substring(0, modifiedFilename.indexOf('.'))+"\\"+modifiedFilename);
+		File folder = new File(modifiedPath+modifiedFilename.substring(0, periodIndex));
+		File toAdd = new File(modifiedPath+modifiedFilename.substring(0, periodIndex)+"\\"+modifiedFilename);
 		if(folder.mkdir()){
 			file.transferTo(toAdd);
 		}else{
@@ -31,8 +44,33 @@ public class FileService {
 			throw new IOException();
 		}
 		
-		//File metaData = new File(D:\\misc games\\savedata\\"+modifiedFilename+"\\"+modifiedFilename)
+		BufferedWriter metaData = new BufferedWriter(new FileWriter(new File(modifiedPath+modifiedFilename.substring(0, periodIndex)+"\\metadata.txt")));
+		metaData.write("Owner "+DELIMITER+" "+owner);
+		metaData.close();
+	}
+	
+	public String getMetaData(String file, String name) throws FileNotFoundException{
+		String modifiedFilename = file.replaceAll("'\'", "");
+		String modifiedPath = getModifiedPath();
+		int periodIndex = modifiedFilename.indexOf('.')==-1?modifiedFilename.length():modifiedFilename.indexOf('.');
+
+		Scanner reader = new Scanner(new File(modifiedPath+modifiedFilename.substring(0, periodIndex)+"\\metadata.txt"));
+		while(reader.hasNextLine()){
+			String current = reader.nextLine();
+			System.out.println(current);
+			String type = current.substring(0, current.indexOf(DELIMITER));
+			if(type.toLowerCase().trim().equals(name.toLowerCase().trim())){
+				reader.close();
+				return current.substring(current.indexOf(DELIMITER)+2);
+			}
+		}
 		
+		reader.close();
+		return null;
+	}
+	
+	private String getModifiedPath(){
+		return System.getenv("FILEPATH").replaceAll("'\'", "\\");
 	}
 	
 }

2b8986ae0a667bc09cc6e28147a77f6cdd4166e4
Brian <cricketfeir@gmail.com>
2017-05-01 07:10:45 -0400

    Removed code to make server run easier on Windows, since code requires ENV

--- src/main/java/example/fileman/Application.java
+++ src/main/java/example/fileman/Application.java
@@ -11,14 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		
-		//This allows a user to run the server from a basic Windows UI without special knowledge
-		JFrame exitGUI = new JFrame("Server");
-		exitGUI.add(new JLabel("Close this window to end the server."));
-		exitGUI.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		exitGUI.pack();
-		exitGUI.setVisible(true);
-		
+				
 		SpringApplication.run(Application.class, args);
 		
 	}
